oracle/linux-uek

crypto: alg - add CRYPTO_ALG_FIPS_PROVIDED flag
Use a new flag to record whether a particular algorithm is provided by
a standalone FIPS 140 module.

Note: This does not mean the algorithm is "FIPS approved" or even "FIPS
allowed" -- it simply means the algorithm is implemented within the FIPS
module.

Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

include/linux/crypto.h
@@ -125,6 +125,14 @@
 #define CRYPTO_ALG_FIPS_INTERNAL	0x00020000
 
 /*
+ * The algorithm is provided by the FIPS module.
+ *
+ * NOTE: an algorithm can be provided by the FIPS module and not be
+ * approved, depending on the exact parameters like key size, etc.
+ */
+#define CRYPTO_ALG_FIPS_PROVIDED	0x00040000
+
+/*
  * Transform masks and values (for crt_flags).
  */
 #define CRYPTO_TFM_NEED_KEY		0x00000001

kernelorg-mirror/linux_kernel_git_vegard_linux-fips140

include/linux/crypto.h
@@ -139,6 +139,14 @@
 /* Set if the algorithm cannot have a fallback (e.g., phmac). */
 #define CRYPTO_ALG_NO_FALLBACK		0x00080000
 
+/*
+ * The algorithm is provided by the FIPS module.
+ *
+ * NOTE: an algorithm can be provided by the FIPS module and not be
+ * approved, depending on the exact parameters like key size, etc.
+ */
+#define CRYPTO_ALG_FIPS_PROVIDED	0x00100000
+
 /* The high bits 0xff000000 are reserved for type-specific flags. */
 
 /*