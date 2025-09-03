1ff0997f040210839bb865650be2f948a8dc79ac
Vegard Nossum <vegard.nossum@oracle.com>
2025-09-03 07:53:11 +0000

    crypto: fips140: convert crypto/ghash-generic.c to using crypto API wrappers
    Use CRYPTO_API() etc. from include/crypto/api.h in preparation for
    compilation as part of support for FIPS 140 standalone modules.
    
    Generated using:
    
      ./fipsify.py --config CONFIG_CRYPTO_GHASH --source crypto/ghash-generic.c
    
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- crypto/ghash-generic.c
+++ crypto/ghash-generic.c
@@ -153,8 +153,8 @@ static void __exit ghash_mod_exit(void)
 	crypto_unregister_shash(&ghash_alg);
 }
 
-module_init(ghash_mod_init);
-module_exit(ghash_mod_exit);
+crypto_module_init(ghash_mod_init);
+crypto_module_exit(ghash_mod_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("GHASH hash function");
