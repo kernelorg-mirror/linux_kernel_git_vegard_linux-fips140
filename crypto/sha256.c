0181ac32714ed30a328c8132098c26cee296f072
Vegard Nossum <vegard.nossum@oracle.com>
2025-09-03 07:53:15 +0000

    crypto: fips140: convert crypto/sha256.c to using crypto API wrappers
    Use CRYPTO_API() etc. from include/crypto/api.h in preparation for
    compilation as part of support for FIPS 140 standalone modules.
    
    Generated using:
    
      ./fipsify.py --config CONFIG_CRYPTO_SHA256 --source crypto/sha256.c
    
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -327,13 +327,13 @@ static int __init crypto_sha256_mod_init(void)
 {
 	return crypto_register_shashes(algs, ARRAY_SIZE(algs));
 }
-module_init(crypto_sha256_mod_init);
+crypto_module_init(crypto_sha256_mod_init);
 
 static void __exit crypto_sha256_mod_exit(void)
 {
 	crypto_unregister_shashes(algs, ARRAY_SIZE(algs));
 }
-module_exit(crypto_sha256_mod_exit);
+crypto_module_exit(crypto_sha256_mod_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Crypto API support for SHA-224, SHA-256, HMAC-SHA224, and HMAC-SHA256");
