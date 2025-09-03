d1a920f9b3b3d4129ce7dacec90ae781a3f7927b
Vegard Nossum <vegard.nossum@oracle.com>
2025-09-03 07:53:17 +0000

    crypto: fips140: convert crypto/simd.c to using crypto API wrappers
    Use CRYPTO_API() etc. from include/crypto/api.h in preparation for
    compilation as part of support for FIPS 140 standalone modules.
    
    Generated using:
    
      ./fipsify.py --config CONFIG_CRYPTO_SIMD --source crypto/simd.c --header include/crypto/internal/simd.h
    
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- crypto/fips140-api.c
+++ crypto/fips140-api.c
@@ -562,3 +562,19 @@ DEFINE_CRYPTO_API_STUB(crypto_grab_sig);
 
 #endif
 
+/*
+ * crypto/simd.c
+ */
+#if !IS_BUILTIN(CONFIG_CRYPTO_SIMD)
+
+#include <crypto/internal/simd.h>
+
+DEFINE_CRYPTO_API_STUB(simd_skcipher_create_compat);
+DEFINE_CRYPTO_API_STUB(simd_skcipher_free);
+DEFINE_CRYPTO_API_STUB(simd_register_skciphers_compat);
+DEFINE_CRYPTO_API_STUB(simd_unregister_skciphers);
+DEFINE_CRYPTO_API_STUB(simd_register_aeads_compat);
+DEFINE_CRYPTO_API_STUB(simd_unregister_aeads);
+
+#endif
+
