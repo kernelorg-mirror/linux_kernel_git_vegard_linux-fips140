a9c67bf4316620b5be7001f95bbda3c769dc3ba5
Vegard Nossum <vegard.nossum@oracle.com>
2025-09-03 07:53:09 +0000

    crypto: fips140: convert crypto/ecdh.c to using crypto API wrappers
    Use CRYPTO_API() etc. from include/crypto/api.h in preparation for
    compilation as part of support for FIPS 140 standalone modules.
    
    Generated using:
    
      ./fipsify.py --config CONFIG_CRYPTO_ECDH --source crypto/ecdh.c
    
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- crypto/ecdh.c
+++ crypto/ecdh.c
@@ -240,8 +240,8 @@ static void __exit ecdh_exit(void)
 	crypto_unregister_kpp(&ecdh_nist_p384);
 }
 
-module_init(ecdh_init);
-module_exit(ecdh_exit);
+crypto_module_init(ecdh_init);
+crypto_module_exit(ecdh_exit);
 MODULE_ALIAS_CRYPTO("ecdh");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("ECDH generic algorithm");
